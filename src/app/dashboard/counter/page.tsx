12f377021ea537defabdea8a672e938a10f4195f
Pablo <juanpablotrujillo95@gmail.com>
2024-04-03 21:13:32 -0500

    fifth section

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,16 @@
+import {Metadata} from "next";
+import {CartCounter} from "@/app/shopping-cart";
+
+export const metadata:Metadata = {
+    title: 'Shopping Cart',
+    description: 'Simple counter page'
+}
+
+export default function CounterPage(){
+    return (
+        <div className="flex flex-col items-center justify-center w-full h-full">
+            <span>Productos en el carrito de compra</span>
+            <CartCounter value={20}/>
+        </div>
+    )
+}
